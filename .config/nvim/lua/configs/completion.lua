d6ef99a847442faf75c385d6ecc4a7535d72342e
elairavi <elairavi@users.noreply.github.com>
2022-06-18 07:47:16 +0100

    nvim: moved link next to icons to make it easier to see

--- .config/nvim/lua/configs/completion.lua
+++ .config/nvim/lua/configs/completion.lua
@@ -22,7 +22,8 @@ local check_backspace = function()
   return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
 end
 
---   פּ ﯟ   some other good icons
+-- Some other good icons   פּ ﯟ  
+-- Find more here: https://www.nerdfonts.com/cheat-sheet
 local kind_icons = {
   Text = "",
   Method = "m",
@@ -50,7 +51,6 @@ local kind_icons = {
   Operator = "",
   TypeParameter = "",
 }
--- find more here: https://www.nerdfonts.com/cheat-sheet
 
 cmp.setup {
   snippet = {
